a04685cf92731b8e6e3e320ef5706c8fda7d4d9c
justinyuen168 <56049841+justinyuen168@users.noreply.github.com>
2019-11-14 13:11:26 -0800

    Update question6.java

--- question6.java
+++ question6.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 class question6 {
     public static void main(String[] args) {
-        Box(500,300000000);
+        Box(50,30);
     }
     
     public static void Box(int width, int height) {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                System.out.print("ha");
+                System.out.print("x");
             }
             System.out.println("");
         }
